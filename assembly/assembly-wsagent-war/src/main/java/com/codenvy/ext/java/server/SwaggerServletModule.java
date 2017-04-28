84a34c28954e27c31559b58bfc584f91ceac72a2
Max Shaposhnik <mshaposhnik@codenvy.com>
2017-04-28 13:40:16 +0000

    CODENVY-2047; fix base path for Swagger configuration on agent (#2149)

--- assembly/assembly-wsagent-war/src/main/java/com/codenvy/ext/java/server/SwaggerServletModule.java
+++ assembly/assembly-wsagent-war/src/main/java/com/codenvy/ext/java/server/SwaggerServletModule.java
@@ -14,7 +14,6 @@
  */
 package com.codenvy.ext.java.server;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.servlet.ServletModule;
 
 import org.eclipse.che.inject.DynaModule;
@@ -24,11 +23,7 @@ import org.eclipse.che.inject.DynaModule;
 public class SwaggerServletModule extends ServletModule {
     @Override
     protected void configureServlets() {
-        bind(io.swagger.jaxrs.config.DefaultJaxrsConfig.class).asEagerSingleton();
         install(new org.eclipse.che.swagger.deploy.DocsModule());
-        serve("/swaggerinit").with(io.swagger.jaxrs.config.DefaultJaxrsConfig.class, ImmutableMap
-                .of("api.version", "1.0",
-                    "swagger.api.title", "Eclipse Che",
-                    "swagger.api.basepath", "/.*/api"));
+        serve("/swaggerinit").with(AgentSwaggerConfig.class);
     }
 }
