7d3e85683bddd040326746e20561398e5a1b0665
Max Shaposhnik <mshaposhnik@codenvy.com>
2017-05-17 14:01:28 +0300

    INFRA-89; do not allow JSESSIONID cookies to be pass between agents;

---
+++ wsagent/codenvy-wsagent-core/src/main/java/com/codenvy/wsagent/server/SetSessionPathListener.java
@@ -0,0 +1,58 @@
+/*
+ *  [2012] - [2017] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.wsagent.server;
+
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Sets correct path to the session id cookies.
+ *
+ * @author Max Shaposhnik (mshaposhnik@codenvy.com)
+ */
+@Singleton
+public class SetSessionPathListener implements ServletContextListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SetSessionPathListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            final ServletContext servletContext = sce.getServletContext();
+            Injector injector = (Injector)servletContext.getAttribute(Injector.class.getName());
+            final String agentEndpoint = injector.getInstance(Key.get(String.class, Names.named("wsagent.endpoint")));
+            servletContext.getSessionCookieConfig().setPath(new URL(agentEndpoint).getPath());
+        } catch (MalformedURLException e) {
+            LOG.warn("Unable to set correct session path due to malformed agent URL.", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
